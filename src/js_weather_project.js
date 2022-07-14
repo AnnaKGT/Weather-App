9baadc1af94a7bd53934c547588ac54a193c6479
AnnaKGT <apk2911@gmail.com>
2022-07-14 19:06:12 +0300

    added relevant icons from API

--- src/js_weather_project.js
+++ src/js_weather_project.js
@@ -1,121 +1,14 @@
-// let weather = {
-//   paris: {
-//     temp: 19.7,
-//     humidity: 80,
-//   },
-//   tokyo: {
-//     temp: 17.3,
-//     humidity: 50,
-//   },
-//   lisbon: {
-//     temp: 30.2,
-//     humidity: 20,
-//   },
-//   "san francisco": {
-//     temp: 20.9,
-//     humidity: 100,
-//   },
-//   oslo: {
-//     temp: -5,
-//     humidity: 20,
-//   },
-// };
-
-// let cityName = prompt("Enter the city");
-// cityName = cityName.trim().toLowerCase();
-
-// let n = 0;
-
-// for (let city in weather) {
-//   if (city === cityName) {
-//     console.log(weather[city].temp);
-//     cityName = city.charAt(0).toUpperCase() + city.slice(1);
-//     alert(
-//       `It is currently ${weather[city].temp}°C (${
-//         weather[city].temp + 32
-//       }°F) in ${cityName} with a humidity of ${weather[city].humidity}%`
-//     );
-//     n = n + 1;
-//   }
-// }
-
-// if (n === 0) {
-//   alert(
-//     `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${cityName}`
-//   );
-// }
-
-// let city = prompt("Enter city");
-// city = city.toLowerCase();
-
-// if (weather[city] !== undefined) {
-//   let humidity = weather[city].humidity;
-//   let temperature = weather[city].temp;
-//   let celsiusTemp = Math.round(temperature);
-//   let fehrenheitTemp = Math.round((temperature * 9) / 5 + 32);
-
-//   alert(
-//     `It is currently ${celsiusTemp}°C (${fehrenheitTemp}°F) in ${city} with a humidity of ${humidity}%`
-//     );
-// } else {
-//   alert(
-//     `Sorry, we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
-//     );
-//   }
-
-// conversion temperature metrics
-// function conversionF() {
-//   let tempCurrentMax = document.querySelector("#temp-current-max");
-//   let tempCurrentMin = document.querySelector("#temp-current-min");
-//   let tempCurrentFeels = document.querySelector("#temp-current-feels");
-//   let temperatureMax = 21;
-//   let temperatureMin = 14;
-//   let temperatureFeels = 24;
-//   let fehrenheitTempMax = Math.round((temperatureMax * 9) / 5 + 32);
-//   let fehrenheitTempMin = Math.round((temperatureMin * 9) / 5 + 32);
-//   let fehrenheitTempFeels = Math.round((temperatureFeels * 9) / 5 + 32);
-
-//   tempCurrentMax.innerHTML = fehrenheitTempMax;
-//   tempCurrentMin.innerHTML = fehrenheitTempMin;
-//   tempCurrentFeels.innerHTML = fehrenheitTempFeels;
-//   debugger;
-//   let metricC = document.querySelector(".C");
-//   let metricF = document.querySelector(".F");
-
-//   metricC.classList.toggle("metricColor");
-//   metricF.classList.toggle("metricColor");
-// }
-
-// function conversionC() {
-//   let tempCurrentMax = document.querySelector("#temp-current-max");
-//   let tempCurrentMin = document.querySelector("#temp-current-min");
-//   let tempCurrentFeels = document.querySelector("#temp-current-feels");
-//   let temperatureMax = 70;
-//   let temperatureMin = 57;
-//   let temperatureFeels = 75;
-//   let fehrenheitTempMax = Math.round(((temperatureMax - 32) * 5) / 9);
-//   let fehrenheitTempMin = Math.round(((temperatureMin - 32) * 5) / 9);
-//   let fehrenheitTempFeels = Math.round(((temperatureFeels - 32) * 5) / 9);
-
-//   tempCurrentMax.innerHTML = fehrenheitTempMax;
-//   tempCurrentMin.innerHTML = fehrenheitTempMin;
-//   tempCurrentFeels.innerHTML = fehrenheitTempFeels;
-
-//   let metricC = document.querySelector(".C");
-//   let metricF = document.querySelector(".F");
-
-//   metricF.classList.toggle("metricColor");
-//   metricC.classList.toggle("metricColor");
-// }
-
-// let metricConverF = document.querySelector(".F");
-// let metricConverC = document.querySelector(".C");
-
-// metricConverF.addEventListener("click", conversionF);
-// metricConverC.addEventListener("click", conversionC);
-
 // Format current date
 function getCurrentDay(dateFormat) {
+  let date = new Date(dateFormat);
+  let time = date.getHours();
+  if (time < 10) {
+    time = `0${time}`;
+  }
+  let min = date.getMinutes();
+  if (min < 10) {
+    min = `0${min}`;
+  }
   let days = [
     "Sunday",
     "Monday",
@@ -125,49 +18,22 @@ function getCurrentDay(dateFormat) {
     "Friday",
     "Satruday",
   ];
+  let day = days[date.getDay()];
 
-  let months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-
-  let day = days[dateFormat.getDay()];
-  let month = months[dateFormat.getMonth()];
-  let date = dateFormat.getDate();
-  let time = dateFormat.getHours();
-  if (time < 10) {
-    time = `0${time}`;
-  }
-
-  let min = dateFormat.getMinutes();
-  if (min < 10) {
-    min = `0${min}`;
-  }
   return `${day} ${time}:${min}`;
 }
 
-let currentDay = document.querySelector("#current-day");
-currentDay.innerHTML = getCurrentDay(new Date());
-
 // search city
 function showData(response) {
+  let dateElement = document.querySelector("#current-day");
   let tempCurrentMax = document.querySelector("#temp-current-max");
   let tempCurrentMin = document.querySelector("#temp-current-min");
   let tempCurrentFeels = document.querySelector("#temp-current-feels");
   let currentHumidity = document.querySelector("#humidity");
   let currentWind = document.querySelector("#wind");
-  let currentWeaterDes = document.querySelector("#weater-description");
+  let currentWeatherDes = document.querySelector("#weater-description");
   let currentCity = document.querySelector("#current-city");
+  let currentIcon = document.querySelector("#current-icon");
 
   tempCurrentMax.innerHTML = Math.round(response.data.main.temp_max);
   tempCurrentMin.innerHTML = Math.round(response.data.main.temp_min);
@@ -175,15 +41,35 @@ function showData(response) {
   currentHumidity.innerHTML = Math.round(response.data.main.humidity);
   currentWind.innerHTML = Math.round(response.data.wind.speed);
   currentCity.innerHTML = response.data.name;
-  currentWeaterDes.innerHTML = response.data.weather[0].main;
-
-  console.log(response.data);
+  currentWeatherDes.innerHTML = response.data.weather[0].main;
+  dateElement.innerHTML = getCurrentDay(response.data.dt * 1000);
+  currentIcon.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  currentIcon.setAttribute(
+    "alt",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].main}@2x.png`
+  );
 }
 
 function searchCity(event) {
   event.preventDefault();
   let currentCity = document.querySelector("#current-city");
   let searchCity = document.querySelector("#search-city");
+  let city1 = document.querySelector("#city-1");
+  let city2 = document.querySelector("#city-2");
+  let city3 = document.querySelector("#city-3");
+  let city4 = document.querySelector("#city-4");
+  let city5 = document.querySelector("#city-5");
+  let city6 = document.querySelector("#city-6");
+
+  city6.innerHTML = city5.innerHTML;
+  city5.innerHTML = city4.innerHTML;
+  city4.innerHTML = city3.innerHTML;
+  city3.innerHTML = city2.innerHTML;
+  city2.innerHTML = city1.innerHTML;
+  city1.innerHTML = currentCity.innerHTML;
   currentCity.innerHTML = searchCity.value;
 
   let apiKey = "1fd9d0abbac5edf293ecf453793c7cfa";
